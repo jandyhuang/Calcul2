b31a934857d617ad28ef83803e39facdb96e9704
Kewei Ge <kewei.ice@gmail.com>
2013-11-17 15:16:44 -0500

    added comments for ast.ml

--- Source Code/ast.ml
+++ Source Code/ast.ml
@@ -1,11 +1,15 @@
+(* binary operators *)
 type op = Add | Sub | Mult | Div | Pow | IntDiv | Mod | Equal | Deriv | Integ | Eq | Neq | Less | Leq |
 Greater | Geq | And | Or
 
+(* unary operators *)
 type preop = Sqrt | Sin | Cos | Tan | ASin | ACos | ATan | Log | Ln | Not
 
+(* vector type: used for derivation *)
 type vector =
     Vector of float list
 
+(* expressions *)
 type expr =
     Num of int
   | Real of float
@@ -16,6 +20,7 @@ type expr =
   | Call of string * expr list
   | Noexpr
 
+(* statements *)
 type stmt =
     Block of stmt list
   | Expr of expr
@@ -24,17 +29,20 @@ type stmt =
   | For of expr * expr * expr * stmt
   | While of expr * stmt
 
+(* declaration for varibles *)
 type var_decl = {
     name : string;
     value : float;
 }
 
+(* declaration for math functions *)
 type math_func_decl = {
     fname : string;
     uknowns : expr list;
     formula : expr list;
 }
 
+(* declaration for functions *)
 type func_decl = {
     fname : string;
     formals : string list;
@@ -42,4 +50,5 @@ type func_decl = {
     body : stmt list;
 }
 
+(* definition for type of program *)
 type program = string list * func_decl list
