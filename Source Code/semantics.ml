f45a0d84a7443f76d19aa409dac4545a6324f77f
michaelzs <shuzhan.swjtu@gmail.com>
2013-12-12 17:49:35 -0500

    semantics modified

--- Source Code/semantics.ml
+++ Source Code/semantics.ml
@@ -30,3 +30,81 @@ let return_func_given_name name env =
 		   let result = List.find (func_equal_name name) env.functions in
 			      result
   with Not_found -> raise(Failure("Function "^ name ^ " has not been declared!"))
+  
+  (*check whether a 'fpname' is in the formal parameter list of a function*)
+let exist_formal_para func fpname = 
+	let check func fpname = List.exists (function a -> a = fpname) func.formals in
+	  check func fpname
+	  
+(*check whether a 'vname' is declared in the global variable list*)
+		
+let exist_global_variable env vname = 
+	let check env vname = List.exists (function a -> a= vname) env.variables in
+	  check env vname
+	  
+	  
+(*check the first existence*)
+
+
+
+(*A function to check whether a function has a parameter called fpname*)
+let check_exist_para_in_fun func fpname = List.exists (function a->a = fpname) func.formals
+
+
+(*The function will check whether a function name "fun_name" has a corresponding function in the environment*)
+
+let find_func func_name env = 
+	try 
+		 let _ = List.find (func_equal_name func_name) env.functions in
+		      true
+	with Not_found -> raise(Failure("Function "^ func_name ^" is not found in the function defination list"))
+	
+	(*This function will check whether a (var_type*string) has a para_name that appears more than once in a function's parameter list*)	
+let count_fpara func = function a
+  -> let f count b = 
+		  if b=a then count+1
+			else count
+			in
+			  let count = List.fold_left f 0 func.formals in
+			     if count > 1
+					then raise(Failure("Duplicate parameter in function " ^ func.fname))
+					else
+						count
+
+
+(*This function will automatically check whether there is parameter duplication in function defination*)
+						
+let check_fpara_duplicate func = 
+	List.map (count_fpara func) func.formal_list
+	
+	(*This function will automatically check whether there is parameter duplication in function defination*)
+						
+let check_fpara_duplicate func = 
+	List.map (count_fpara func) func.formal_list  
+
+(*This function will check whether a (var_type*string) has a var_name that appears more than once in a function's local variable list*)
+let count_var func = function (_,b)
+  -> let f count (_,c) = 
+		  if c=b then count+1
+			else count
+			in
+			  let count = List.fold_left f 0 func.locals in
+			     if count > 1
+					then raise(Failure("Duplicate parameter "^ b ^ " in function " ^ func.fname))
+					else
+						count
+						
+(*The following function will judge whether an expression is assign or call*)
+
+let is_assign_call func = function
+	| Assign(_,_) ->true
+	| Call(_,_) ->true
+	| _ ->false
+	
+	
+(*The following function will judge whether an expression is assign or call*)
+
+let is_assign_call func = function
+	| Assign(_,_) ->true
+	| Call(_,_) ->true
+	| _ ->false
