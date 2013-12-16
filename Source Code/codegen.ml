6e0ec4ed85ec0acda121fa99f522e23f65b13ec7
wingsking <jz2540@columbia.edu>
2013-12-15 19:29:11 -0500

    part of codegen, still need works(read comments)

--- Source Code/codegen.ml
+++ Source Code/codegen.ml
@@ -1,1 +1,60 @@
 open Ast
+open Printf
+
+
+let rec gen_expr = function
+    Real(l) -> string_of_float l
+  | Id(s) -> s
+  | PreUnaop(preop, e) ->
+	  "" ^
+	  (match preop with
+	  Sqrt -> "sqrt" | Sin -> "sin" | Cos -> "cos" | Tan -> "tan" | ASin -> "asin"
+	  | ACos -> "acos" | ATan -> "atan" | Log -> "log" | Ln -> "ln" | Not -> "!")
+	  ^ "(" ^ gen_expr e ^ ")"
+  | Binop(e1, o, e2) ->
+      gen_expr e1 ^ " " ^
+      (match o with
+		Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
+	  | And -> "&&" | Or -> "||" | Eq -> "==" | Neq -> "!="
+      | Less -> "<" | Leq -> "<=" | Greater -> ">" | Geq -> ">="
+	  
+	  |	Pow -> "pow" | Deriv -> "'" | Integ -> "@"				(*Pow and Deriv and Integ to be determined*)
+	  
+	  ) ^ " " ^
+      gen_expr e2
+  | Assign(v, e) -> v ^ " = " ^ gen_expr e
+  | Call(fname, el) -> fname ^ "(" ^ String.concat ", " (List.map gen_expr el) ^ ")"
+  | Noexpr -> ""
+  
+  
+(* wait to be determined *) 
+let gen_math (fname, unknowns, formula)= 
+	fname ^ "(" ^ String.concat ", " unknowns ^ "){\n" ^ (gen_expr formula) ^ "}\n"
+  
+  
+let rec gen_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map gen_stmt stmts) ^ "}\n"
+  | Expr(expr) -> gen_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ gen_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ gen_expr e ^ ")\n" ^ gen_stmt s
+  | If(e, s1, s2) ->  "if (" ^ gen_expr e ^ ")\n" ^
+      gen_stmt s1 ^ "else\n" ^ gen_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ gen_expr e1  ^ " ; " ^ gen_expr e2 ^ " ; " ^
+      gen_expr e3  ^ ") " ^ gen_stmt s
+  | While(e, s) -> "while (" ^ gen_expr e ^ ") " ^ gen_stmt s
+  
+  | Output(e) -> "cout<<" ^ gen_expr e ^ ";\n"
+  | Math_func(s, sl, e) -> gen_math (s, sl, e)
+  
+(* needed to work on it *)  
+let gen_vdecl (name, expr)= "float " ^ name ^ "=" ^ String.concat "" expr ^";\n"
+
+let gen_fdecl fdecl =
+  fdecl.fname ^ "(" ^ String.concat ", " fdecl.formals ^ "){\n" ^
+  String.concat "" (List.map gen_stmt fdecl.body) ^
+  "}\n"
+
+let gen_program prog =
+  String.concat "" (List.map gen_fdecl prog)
