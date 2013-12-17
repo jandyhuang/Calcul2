7c6653a83d818f24cdb6ab2cc0996553280360fa
jandyhuang <jndhuang8@gmail.com>
2013-12-17 02:30:53 -0500

    Update calcul2.h
    Derivation added. Fixed every problem known. Much more powerful than I
    can ever imagine.

--- Source Code/calcul2.h
+++ Source Code/calcul2.h
@@ -26,7 +26,7 @@ using namespace std;
 #define LOG	15
 #define LN	16
 
-#define TOT	1e6
+#define TOT	1e5
 
 int sgn(double v){
 	if(fabs(v)<1e-8)return 0;
@@ -76,6 +76,7 @@ private:
 	FNode *left, *right;
 public:
 	FNode(int _type, double _value, int _op, FNode *_left, FNode *_right);
+	FNode *Copy();
 	bool IsSame(FNode *tre);
 	FNode* GetLeft();	
 	FNode* GetRight();
@@ -85,13 +86,19 @@ public:
 	void Print(int LastOp, vector<string>variable);
 	double GetValue(vector<double>_var);
 	void Reduce();
-	FNode*Deride(int _var);
+	FNode*Derive(int _var);
 	bool AddNode(FNode*node);
 };
 
-
 FNode::FNode(int _type=0, double _value=0, int _op=0, FNode *_left=NULL, FNode*_right=NULL):type(_type),
 	value(_value),op(_op),left(_left),right(_right){}
+
+FNode *FNode::Copy(){
+	FNode *tmp=new FNode(type,value,op);
+	if(left!=NULL)tmp->left=left->Copy();
+	if(right!=NULL)tmp->right=right->Copy();
+	return tmp;
+}
 
 bool FNode::IsSame(FNode *tre){
 	if((left==NULL) ^ (tre->left==NULL))return false;
@@ -131,11 +138,18 @@ void FNode::SetRight(FNode* _right){
 
 void FNode::Print(int LastOp, vector<string>variable){
 	if(type==T_VAL){
-		printf("%.3lf", value);
+		if(sgn(value-int(value+1e-8))==0 || sgn(value-int(value-1e-8))==0)
+			printf("%.0lf",value);
+		else printf("%.3lf", value);
 	}else if(type==T_VAR){
 		printf("%s", variable[op].c_str());
 	}else{
-		if(LastOp>=SQRT || op>=PLUS && op<=POWER && LastOp>=PLUS && LastOp<=POWER && (LastOp>op || LastOp==POWER) && (LastOp+1)/2>=(op+1)/2){
+		if(op>POWER){
+			PrintOp(op);
+			printf("(");
+			if(right!=NULL)right->Print(op,variable);
+			printf(")");
+		}else if(op>=PLUS && op<=POWER && LastOp>=PLUS && LastOp<=POWER && (LastOp>op || LastOp==POWER) && (LastOp+1)/2>=(op+1)/2){
 			printf("(");
 			if(op>=PLUS && op<=POWER && left!=NULL)left->Print(op,variable);
 			PrintOp(op);
@@ -143,7 +157,7 @@ void FNode::Print(int LastOp, vector<string>variable){
 			printf(")");
 		}
 		else{
-			if(op>=PLUS && op<=POWER && left!=NULL)left->Print(op,variable);
+			if(left!=NULL)left->Print(op,variable);
 			PrintOp(op);
 			if(right!=NULL)right->Print(op,variable);
 		}
@@ -254,6 +268,13 @@ void FNode::Reduce(){
 			delete(tmp);
 			return;
 		}
+		if(op==PLUS && left->type==T_VAL && sgn(left->value)==0){
+			left->Destroy();
+			FNode*tmp=right;
+			(*this)=(*tmp);
+			delete(tmp);
+			return;
+		}
 		if(op==MINUS && left->IsSame(right)){
 			type=T_VAL;
 			value=0;
@@ -269,7 +290,7 @@ void FNode::Reduce(){
 			delete(tmp);
 			return;
 		}
-		if(op==TIMES && right->type==T_VAL && sgn(right->value)==0){
+		if(op==TIMES && (left->type==T_VAL && sgn(left->value)==0 || right->type==T_VAL && sgn(right->value)==0)){
 			type=T_VAL;
 			value=0;
 			left->Destroy();
@@ -284,6 +305,13 @@ void FNode::Reduce(){
 			delete(tmp);
 			return;
 		}
+		if(op==TIMES && left->type==T_VAL && sgn(left->value-1)==0){
+			left->Destroy();
+			FNode*tmp=right;
+			(*this)=(*tmp);
+			delete(tmp);
+			return;
+		}
 		if(op==TIMES && left->IsSame(right)){
 			op=POWER;
 			right->type=T_VAL;
@@ -294,6 +322,13 @@ void FNode::Reduce(){
 			return;
 		}
 		if(op==DIVIDE && right->type==T_VAL && sgn(right->value-1)==0){
+			right->Destroy();
+			FNode*tmp=left;
+			(*this)=(*tmp);
+			delete(tmp);
+			return;
+		}
+		if(op==DIVIDE && left->type==T_VAL && sgn(left->value)==0){
 			right->Destroy();
 			FNode*tmp=left;
 			(*this)=(*tmp);
@@ -323,11 +358,154 @@ void FNode::Reduce(){
 			left=right=NULL;
 			return;
 		}
+
+		if((left==NULL || left->type==T_VAL) && right->type==T_VAL){
+			type=T_VAL;
+			vector<double> empty;
+			value=GetValue(empty);
+			left->Destroy();
+			right->Destroy();
+			left=right=NULL;
+			return;
+		}
 	}
 }
 
-FNode* FNode::Deride(int _var){
-	return NULL;
+FNode* FNode::Derive(int _var){
+	FNode *tmp=new FNode;
+	if(type==T_VAL){
+		tmp->type=T_VAL;
+		tmp->value=0;
+	}else if(type==T_VAR){
+		tmp->type=T_VAL;
+		tmp->value=(_var==op?1:0);
+	}else{
+		tmp->type=T_OP;
+		switch(op){
+			case PLUS:
+				tmp->op=PLUS;
+				tmp->left=left->Derive(_var);
+				tmp->right=right->Derive(_var);
+				break;
+			case MINUS:
+				tmp->op=MINUS;
+				tmp->left=left->Derive(_var);
+				tmp->right=right->Derive(_var);
+				break;
+			case TIMES:
+				tmp->op=PLUS;
+				tmp->left=new FNode(T_OP,0,TIMES);
+				tmp->left->left=left->Derive(_var);
+				tmp->left->right=right->Copy();
+				tmp->right=new FNode(T_OP,0,TIMES);
+				tmp->right->left=left->Copy();
+				tmp->right->right=right->Derive(_var);
+				break;
+			case DIVIDE:
+				tmp->op=DIVIDE;
+				tmp->left=new FNode(T_OP,0,MINUS);
+				tmp->left->left=new FNode(T_OP,0,TIMES);
+				tmp->left->left->left=left->Derive(_var);
+				tmp->left->left->right=right->Copy();
+				tmp->left->right=new FNode(T_OP,0,TIMES);
+				tmp->left->right->left=left->Copy();
+				tmp->left->right->right=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,POWER);
+				tmp->right->left=right->Copy();
+				tmp->right->right=new FNode(T_VAL,2);
+				break;
+			case POWER:
+				tmp->op=TIMES;
+				tmp->left=new FNode(T_OP,0,POWER);
+				tmp->left->left=left->Copy();
+				tmp->left->right=right->Copy();
+				tmp->right=new FNode(T_OP,0,PLUS);
+				tmp->right->left=new FNode(T_OP,0,DIVIDE);
+				tmp->right->left->left=new FNode(T_OP,0,TIMES);
+				tmp->right->left->left->left=right->Copy();
+				tmp->right->left->left->right=left->Derive(_var);
+				tmp->right->left->right=left->Copy();
+				tmp->right->right=new FNode(T_OP,0,TIMES);
+				tmp->right->right->left=new FNode(T_OP,0,LN);
+				tmp->right->right->left->right=left->Copy();
+				tmp->right->right->right=right->Derive(_var);
+				break;
+			case SQRT:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,TIMES);
+				tmp->right->left=new FNode(T_VAL,2);
+				tmp->right->right=new FNode(T_OP,0,SQRT);
+				tmp->right->right->right=right->Copy();
+				break;
+			case SIN:
+				tmp->op=TIMES;
+				tmp->left=new FNode(T_OP,0,COS);
+				tmp->left->right=right->Copy();
+				tmp->right=right->Derive(_var);
+				break;
+			case COS:
+				tmp->op=TIMES;
+				tmp->left=new FNode(T_OP,0,TIMES);
+				tmp->left->left=new FNode(T_VAL,-1);
+				tmp->left->right=new FNode(T_OP,0,SIN);
+				tmp->left->right->right=right->Copy();
+				tmp->right=right->Derive(_var);
+				break;
+			case TAN:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,POWER);
+				tmp->right->left=new FNode(T_OP,0,COS);
+				tmp->right->left->right=right->Copy();
+				tmp->right->right=new FNode(T_VAL,2);
+				break;
+			case ASIN:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,SQRT);
+				tmp->right->right=new FNode(T_OP,0,MINUS);
+				tmp->right->right->left=new FNode(T_VAL,1);
+				tmp->right->right->right=new FNode(T_OP,0,POWER);
+				tmp->right->right->right->left=right->Copy();
+				tmp->right->right->right->right=new FNode(T_VAL,2);
+				break;
+			case ACOS:
+				tmp->op=DIVIDE;
+				tmp->left=new FNode(T_OP,0,TIMES);
+				tmp->left->left=new FNode(T_VAL,-1);
+				tmp->left->right=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,SQRT);
+				tmp->right->right=new FNode(T_OP,0,MINUS);
+				tmp->right->right->left=new FNode(T_VAL,1);
+				tmp->right->right->right=new FNode(T_OP,0,POWER);
+				tmp->right->right->right->left=right->Copy();
+				tmp->right->right->right->right=new FNode(T_VAL,2);
+				break;
+			case ATAN:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,PLUS);
+				tmp->right->left=new FNode(T_VAL,1);
+				tmp->right->right=new FNode(T_OP,0,POWER);
+				tmp->right->right->left=right->Copy();
+				tmp->right->right->right=new FNode(T_VAL,2);
+				break;
+			case LOG:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=new FNode(T_OP,0,TIMES);
+				tmp->right->left=right->Copy();
+				tmp->right->right=new FNode(T_VAL,log(2.0));
+				break;
+			case LN:
+				tmp->op=DIVIDE;
+				tmp->left=right->Derive(_var);
+				tmp->right=right->Copy();
+				break;
+		}
+	}
+	return tmp;
 }
 
 bool FNode::AddNode(FNode*node){
@@ -353,13 +531,18 @@ private:
 	double GetIntegal(vector<double>_begin,vector<double>_end,vector<double>_now,int _var);
 public:
 	FTree(vector<string>_variable, FNode*_root);
+	FNode*Copy();
 	void AddNode(FNode*node);
 	void Print();
-	void Deride(int _var);
+	FTree*Derive(int _var);
 	double GetIntegal(vector<double>_begin, vector<double>_end);
 	double GetValue(vector<double>_var);
 	void Reduce();
 };
+
+FNode*FTree::Copy(){
+	return root->Copy();
+}
 
 void FTree::Reduce(){
 	root->Reduce();
@@ -376,8 +559,12 @@ void FTree::Print(){
 	root->Print(-1,variable);
 }
 
-void FTree::Deride(int _var){
-	root->Deride(_var);
+FTree* FTree::Derive(int _var){
+	vector<string> tmp_variable;
+	tmp_variable.push_back(variable[_var]);
+	FTree*tmp=new FTree(tmp_variable,root->Derive(_var));
+	tmp->root->Reduce();
+	return tmp;
 }
 
 double FTree::GetValue(vector<double>_var){
