60495bcba0fc3e60cca470a3060c8b87719d3187
Kewei Ge <kewei.ice@gmail.com>
2013-12-17 01:26:30 -0500

    Updated codegen.ml
    Added code generation for math functions.
    
    Now it can generate cpp code correctly, which matches the data
    structure that our cpp library uses.
    
    Here is an example:
    1. Input:
    main()
    {
    	f($x, $y) = 2 * x ^ 2 + y * 3;
    	:f(1, 2);
    }
    
    2. Generated:
    
    #include <cstdio>
    #include "calcul2.h"
    using namespace std;
    
    int main()
    {
    	double printer;
    
    	vector<string> f_var;
    	f.push_back("x");
    	f.push_back("y");
    	vector<double> f_now;
    	FTree f(f_var);
    
    	f.AddNode(new FNode(T_VAL,2.));
    	f.AddNode(new FNode(T_OP,0,TIMES));
    	f.AddNode(new FNode(T_VAR,0,0));
    	f.AddNode(new FNode(T_OP,0,POWER));
    	f.AddNode(new FNode(T_VAL,2.));
    	f.AddNode(new FNode(T_OP,0,PLUS));
    	f.AddNode(new FNode(T_VAR,0,1));
    	f.AddNode(new FNode(T_OP,0,TIMES));
    	f.AddNode(new FNode(T_VAL,3.));
    
    	f_now.clear();
    	f_now.pushback("1.");
    	f_now.pushback("2.");
    	printer = f.GetValue(f_now);
    	printf("%lf\n",printer);
    
    	return 0;
    }

--- Source Code/codegen.ml
+++ Source Code/codegen.ml
@@ -29,12 +29,80 @@ let rec gen_expr = function
   | Call(fname, el) -> fname ^ "(" ^ String.concat ", " (List.map gen_expr el) ^ ")"
   | Noexpr -> ""
   
-  
+(* help to generate f.push_back("unknowns") *)
+let rec pad (unks, fname) =
+  match unks with
+    [] -> []
+  | u :: tl -> (fname^".push_back(\""^u^"\");") :: (pad (tl, fname))
+
+let rec get_index unk = function
+    [] -> 0
+  | h :: t -> if h = unk then 0 else 1 + get_index unk t
+
+let new_node fname decls =
+  fname ^ ".AddNode(new FNode(" ^ decls ^ "));\n\t"
+
+let rec gen_tree_op (fname, unknowns) = function
+    Real(l) -> new_node fname ("T_VAL,"^(string_of_float l))
+  | Id(s) -> new_node fname ("T_VAR,0,"^(string_of_int (get_index s unknowns)))
+  | Binop(e1, o, e2) -> 
+      gen_tree_op (fname, unknowns) e1 ^
+        ( match o with
+            Add -> new_node fname "T_OP,0,PLUS" 
+          | Sub -> new_node fname "T_OP,0,MINUS"
+          | Mult -> new_node fname "T_OP,0,TIMES"
+          | Div -> new_node fname "T_OP,0,DIVIDE"
+          | And -> new_node fname "T_OP,0,AND"
+          | Or -> new_node fname "T_OP,0,OR"
+          | Eq -> new_node fname "T_OP,0,EQUAL"
+          | Neq -> new_node fname "T_OP,0,NOTEQUAL"
+          | Less -> new_node fname "T_OP,0,LESS"
+          | Leq -> new_node fname "T_OP,0,LESSEQUAL"
+          | Greater -> new_node fname "T_OP,0,GREATER"
+          | Geq -> new_node fname "T_OP,0,GREATEREQUAL"
+          | Pow -> new_node fname "T_OP,0,POWER"
+          | Deriv -> new_node fname "T_OP,0,DERIV"
+          | Integ -> new_node fname "T_OP,0,INTEG"
+        )
+      ^ gen_tree_op (fname, unknowns) e2
+  | Noexpr -> ""
+
+let rec construct_tree (fname, unknowns, formula) =
+  gen_tree_op (fname, unknowns) formula
+
+let add_fname (fname, unknowns) = 
+  let dcl_fname = "vector<string> "^fname^"_var;\n\t" in
+  let pbk_fname = String.concat "\n\t" (pad (unknowns, fname)) in
+  let dcl_fval = "vector<double> "^fname^"_now;\n\t" in
+  let dcl_tree = "FTree "^fname^"("^fname^"_var);\n\t" in 
+    dcl_fname ^ pbk_fname ^ "\n\t" ^ dcl_fval ^ dcl_tree ^ "\n\t"
+
+let add_tree (fname, unknowns, formula) =
+  let cst_tree = construct_tree (fname, unknowns, formula) in
+    cst_tree
+  (*create_name^push_back*)
+
+let rec pad_call (el, fname) =
+  match el with
+    [] -> []
+  | u :: tl -> (fname^"_now.push_back(\""^u^"\");") :: (pad_call (tl, fname))
+
+let rec gen_value fname = function
+    Real(l) -> fname^"_now.pushback(\""^(string_of_float l)^"\");\n\t"
+
+let rec gen_call_func = function
+    Call(fname, el) -> fname ^ "_now.clear();\n\t" ^ String.concat "" (List.map (gen_value fname) el)
+      ^ "printer = " ^ fname ^ ".GetValue(" ^ fname ^ "_now);\n\t" 
+      ^ "printf(\"%lf\\n\",printer);\n\t"
+  | _ -> ""
+
 (* wait to be determined *) 
 let gen_math (fname, unknowns, formula)= 
-	fname ^ "(" ^ String.concat ", " unknowns ^ "){\n\t\t" ^ (gen_expr formula) ^ "\n\t}\n"
-  
-  
+  let addfname = add_fname (fname, unknowns) in
+  let addtree = add_tree (fname, unknowns, formula) in
+    addfname ^ addtree
+	(*fname ^ "(" ^ String.concat ", " unknowns ^ "){\n\t\t" ^ (gen_expr formula) ^ "\n\t}\n"*)
+
 let rec gen_stmt = function
     Block(stmts) ->
       "{\n" ^ String.concat "" (List.map gen_stmt stmts) ^ "}\n"
@@ -48,7 +116,7 @@ let rec gen_stmt = function
       gen_expr e3  ^ ") " ^ gen_stmt s
   | While(e, s) -> "while (" ^ gen_expr e ^ ") " ^ gen_stmt s
   
-  | Output(e) -> "cout<<" ^ gen_expr e ^ ";\n"
+  | Output(e) -> gen_call_func e
   | Math_func(s, sl, e) -> gen_math (s, sl, e)
   
 (* needed to work on it *)  
@@ -60,7 +128,7 @@ let gen_fdecl fdecl =
   let formal_list = String.concat ", " fdecl.formals in
   let body = String.concat "\n\t" (List.map gen_stmt fdecl.body) in
     match fname with
-        "main" -> ftype^" "^fname^"("^formal_list^")\n{\n\tdouble printer;\n\t"^body^"\n\tprintf(\"%lf\\n\",printer);\n\treturn 0;\n}\n"
+        "main" -> ftype^" "^fname^"("^formal_list^")\n{\n\tdouble printer;\n\n\t"^body^"\n\treturn 0;\n}\n"
       | _ -> ftype^" "^fname^"("^formal_list^")\n{\n\t"^body^"\n}\n"
 
 let gen_program prog =
