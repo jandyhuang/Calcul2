d5c8879d04a65d82a2ce23cc806acb4296413591
Kewei Ge <kewei.ice@gmail.com>
2013-12-18 21:54:55 -0500

    Updated codegen.ml, compiler.ml, makefile.
    Added comments for codegen.ml.

--- Source Code/codegen.ml
+++ Source Code/codegen.ml
@@ -1,13 +1,15 @@
 open Ast
 open Printf
 
+(* generate the return type of a function *)
 let gen_type = function
     "main" -> "int"
   | _ -> "double"
 
-
+(* variable list inside a function *)
 let var_list = ref []
 
+(* math function list inside a function *)
 let mathf_list = ref []
 
 let exist_id id id_list= List.exists (function x -> x = id) !id_list
@@ -17,7 +19,7 @@ let gen_var var =
 	true -> var
   | false -> ignore(var_list := var :: !var_list); "double "^var
   
-  
+(* generate the cpp code for expression *)
 let rec gen_expr = function
     Real(l) -> string_of_float l
   | Id(s) -> gen_var s
@@ -35,7 +37,7 @@ let rec gen_expr = function
             Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
           | And -> "&&" | Or -> "||" | Eq -> "==" | Neq -> "!="
       	  | Less -> "<" | Leq -> "<=" | Greater -> ">" | Geq -> ">="
-          
+          | _ -> ""
           (*| Pow -> "pow" | Deriv -> "'" | Integ -> "@" *)   (*Pow and Deriv and Integ to be determined*)
           
           ) ^ " " ^ gen_expr e2
@@ -50,21 +52,26 @@ let rec pad (unks, fname) =
     [] -> []
   | u :: tl -> (fname^"_var.push_back(\""^u^"\");") :: (pad (tl, fname))
 
+(* get the index of a given unknown *)
 let rec get_index unk = function
     [] -> 0
   | h :: t -> if h = unk then 0 else 1 + get_index unk t
 
+(* pad the new node code for a given function with declarations *)
 let new_node fname decls =
   fname ^ ".AddNode(new FNode(" ^ decls ^ "));\n\t"
 
+(* pad the add node code for a given function with declarations *)
 let add_node fname decls =
   fname ^ ".AddNode(" ^ decls ^ ");\n\t"
 
+(* generate the math arguments *)
 let gen_math_args = function
     Id(s) -> s
   | Real(l) -> string_of_float l
   | _ -> ""
 
+(* recursion function to generate the tree operator structures *)
 let rec gen_tree_op (fname, unknowns) = function
     Real(l) -> new_node fname ("T_VAL,"^(string_of_float l))
   | Id(s) -> 
@@ -89,6 +96,7 @@ let rec gen_tree_op (fname, unknowns) = function
           | Pow -> new_node fname "T_OP,0,POWER"
           | Deriv -> add_node fname (gen_math_args e1 ^ "." ^ "Derive(\"" 
                      ^ gen_math_args e2 ^ "\") -> Copy()")
+          | _ -> ""
           (*| Integ -> new_node fname "T_OP,0,INTEG"*)
         )
         ^ 
@@ -112,10 +120,13 @@ let rec gen_tree_op (fname, unknowns) = function
         )
         ^ gen_tree_op (fname, unknowns) e
   | Noexpr -> ""
+  | _ -> ""
 
+(* construct the tree structure *)
 let rec construct_tree (fname, unknowns, formula) =
   gen_tree_op (fname, unknowns) formula
 
+(* add the function name and tree declarations *)
 let add_fname (fname, unknowns) = 
   let _ = match (exist_id fname mathf_list) with
       false -> ignore(mathf_list := fname :: !mathf_list); "" 
@@ -126,19 +137,23 @@ let add_fname (fname, unknowns) =
   let dcl_tree = "FTree "^fname^"("^fname^"_var);\n\t" in 
     dcl_fname ^ pbk_fname ^ "\n\t" ^ dcl_fval ^ dcl_tree ^ "\n\t"
 
+(* construct the tree *)
 let add_tree (fname, unknowns, formula) =
   let cst_tree = construct_tree (fname, unknowns, formula) in
     cst_tree
-  (*create_name^push_back*)
 
+(* pad the cpp code for push_back *)
 let rec pad_call (el, fname) =
   match el with
     [] -> []
   | u :: tl -> (fname^"_now.push_back(\""^u^"\");") :: (pad_call (tl, fname))
 
+(* generate the cpp code for getting values of a function *)
 let rec gen_value fname = function
     Real(l) -> fname^"_now.push_back("^(string_of_float l)^");\n\t"
+  | _ -> "" 
 
+(* generate the cpp code for setting the integal arguments *)
 let rec gen_begin_end (fname, el) =
     fname ^ "_begin.clear();\n\t" ^ fname ^ "_end.clear();\n\t" ^
     (match el with
@@ -146,13 +161,16 @@ let rec gen_begin_end (fname, el) =
                      fname ^ "_end.push_back(" ^ gen_math_args b ^ ");\n\t"
     | _ -> "")
 
+(* generate the cpp code for setting the integal arguments *)
 let gen_integ fname = function
       Call(unk, el) -> gen_begin_end (fname, el)
     | _ -> ""
 
+(* generate the cpp code for getting the integal value *)
 let get_integ fname =
     fname ^ ".GetIntegal(" ^ fname ^ "_begin, " ^ fname ^ "_end);\n\t"
 
+(* handles a call expression *)
 let rec gen_call_func = function
     Call(fname, el) -> fname ^ "_now.clear();\n\t" ^ String.concat "" (List.map (gen_value fname) el)
       ^ "printer = " ^ fname ^ ".GetValue(" ^ fname ^ "_now);\n\t" 
@@ -172,15 +190,14 @@ let rec gen_call_func = function
   | _ -> "" 
 
 
-
-
-(* wait to be determined *) 
+(* generate the math declaration and create tree structure *)
 let gen_math (fname, unknowns, formula)= 
   let addfname = add_fname (fname, unknowns) in
   let addtree = add_tree (fname, unknowns, formula) in
     addfname ^ addtree
         (*fname ^ "(" ^ String.concat ", " unknowns ^ "){\n\t\t" ^ (gen_expr formula) ^ "\n\t}\n"*)
 
+(* generate the statement *)
 let rec gen_stmt = function
     Block(stmts) ->
       "{\n" ^ String.concat "" (List.map gen_stmt stmts) ^ "}\n"
@@ -197,11 +214,10 @@ let rec gen_stmt = function
   | Output(e) -> gen_call_func e
   | Math_func(s, sl, e) -> gen_math (s, sl, e)
 
-
-  
-
+(* generate the cpp code for formals, if it apears the first time, then give the type definition *)
 let gen_formals formals = ignore(List.map (fun(x)->var_list := x :: !var_list) formals); List.map (fun(l) -> "double "^l) formals  
-	
+
+(* generate the cpp code for function declaration *)
 let gen_fdecl fdecl =
   let ftype = gen_type (fdecl.fname) in
   let fname = fdecl.fname in
@@ -212,8 +228,9 @@ let gen_fdecl fdecl =
         "main" -> ftype^" "^fname^"("^formal_list^")\n{\n\tdouble printer;\n\n\t"^body^"\n\treturn 0;\n}\n"
       | _ -> ftype^" "^fname^"("^formal_list^")\n{\n\t"^body^"\n}\n"
 
+(* entrence of codegen, generate the cpp code for our calcul2 code *)
 let gen_program prog =
   let header = "#include <cstdio>\n#include \"calcul2.h\"\nusing namespace std;\n" in
     let fdecls = String.concat "\n" (List.map gen_fdecl prog) in
-      let _ = print_endline "Code Generation completed successfully.\n" in
+      let _ = print_endline "Codegen completed.\nCompiling..\n" in
         header^"\n"^fdecls
--- Source Code/compiler.ml
+++ Source Code/compiler.ml
@@ -5,6 +5,5 @@ open Printf
         (* let _ = Semantics.check_program parse_prog in*)
         let prog = Codegen.gen_program parse_prog in
         let output_cpp = open_out "output.cpp" in
-        let _ = print_endline prog in
         output_string output_cpp prog
 
