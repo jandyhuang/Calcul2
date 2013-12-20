16cecd62fd6dc3fa8f46705507af41dce3a30542
wingsking <jz2540@columbia.edu>
2013-12-20 16:37:50 -0500

    Update compiler.ml

--- Source Code/compiler.ml
+++ Source Code/compiler.ml
@@ -2,7 +2,7 @@ open Printf
 	let _ =
         let lexbuf = Lexing.from_channel stdin in
         let parse_prog = Parser.program Scanner.token lexbuf in
-        (* let _ = Semantics.check_program parse_prog in*)
+        let _ = Semantics.check_program parse_prog in
         let prog = Codegen.gen_program parse_prog in
         let output_cpp = open_out "output.cpp" in
         output_string output_cpp prog
